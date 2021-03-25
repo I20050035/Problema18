a83013c29a4947b5c937a1e27c026dc3334a3ab7
I20050035 <70992098+I20050035@users.noreply.github.com>
2021-03-24 22:51:08 -0600

    Add files via upload

---
+++ Problema 18/Problema 18/Operaciones.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problema_18
+{
+    class Operaciones
+    {
+        public double Operacionn(double n)
+        {
+
+            double suma = 0;
+            for (double i = 1; i <= n; i++)
+            {
+                double ar = 1;
+                double ab = Math.Pow(n, i);
+                for (double j = 1; j <= i; j++)
+                {
+
+                    ab = ab * j;
+
+
+                }
+                double div = ar / ab;
+                suma = suma + div;
+
+            }
+            suma = suma + 1;
+            return suma;
+        }
+    }
+}
